ce1bbb83a79ee35f0246fcd42dfe42f1f64ae1f3
gavinoconnor <gedmund@gmail.com>
2020-10-28 14:05:26 -0400

    connect to Yelp API

---
+++ src/util/Yelp.js
@@ -0,0 +1,36 @@
+const apiKey = '6OPJa_4uk-56Jl0M1svzt4saYFriFgsqdj9d-RNwZLFijsGd2mSDbk7KfqGCSO4KbZKAnO2bQhVh4cycj2X6Cqkib0AV3_flevoItIeuzfeX3RMdtIP1sJlKQKaZX3Yx';
+
+const Yelp = {
+  search(term, location, sortBy) {
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+      headers: {
+        Authorization: `Bearer ${apiKey}`
+      },
+    })
+    .then(response => {
+      return response.json();
+    })
+    .then(jsonResponse => {
+      if (jsonResponse.businesses) {
+        return jsonResponse.businesses.map(business => {
+          console.log(business)
+          return {
+            id: business.id,
+            imageSrc: business.image_url,
+            name: business.name,
+            address: business.location.address1,
+            city: business.location.city,
+            state: business.location.state,
+            zipCode: business.location.zip_code,
+            category: business.categories[0].title,
+            rating: business.rating,
+            reviewCount: business.review_count
+          }
+        })
+      }
+    })
+  }
+
+};
+
+export default Yelp;
